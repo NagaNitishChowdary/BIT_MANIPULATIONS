80f1a71046b88b1018d5272eac9c0098c20cec91
NagaNitishChowdary <95007559+NagaNitishChowdary@users.noreply.github.com>
2022-08-11 22:01:45 +0530

    Update FINDING NUMBER OCCURED ONCE WHILE OTHER OCCURED TWICE USING XOR.cpp

--- FINDING NUMBER OCCURED ONCE WHILE OTHER OCCURED TWICE USING XOR.cpp
+++ FINDING NUMBER OCCURED ONCE WHILE OTHER OCCURED TWICE USING XOR.cpp
@@ -23,3 +23,34 @@ int main() {
     cin>>testCases;
     while(testCases--) solve();
 }
+
+/*
+
+//FIND NUBMER WHICH APPEARS ONLY ONCE AND REMAINING ALL APPEARS TWICE IN ARRAY
+#include <iostream>
+#include<bits/stdc++.h>
+#define ll long long int 
+using namespace std;
+
+void solve(){
+    ll n;
+    cin>>n;
+    int arr[n];
+    //ll xor=0;
+    for(ll i=0;i<n;i++){
+        cin>>arr[i];
+        //xor=xor^arr[i];
+        }
+    sort(arr,arr+n);
+    ll flag=0;
+    for(ll i=0;i<n-1;i+=2) if(arr[i]!=arr[i+1]){cout<<arr[i];flag=1;break;}
+    if(flag==0) cout<<arr[n-1]<<endl;
+}
+
+int main() {
+    int testCases;
+    cin>>testCases;
+    while(testCases--) solve();
+}
+
+*/
